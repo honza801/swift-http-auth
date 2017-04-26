33d96bdc63c3aa7d642e588b63785d8c22063c8e
Jan Krcmar <honza801@gmail.com>
2017-04-26 14:32:45 +0200

    move functions to correct classes, generate token groups dynamically

--- swift-http-auth.py
+++ swift-http-auth.py
@@ -12,11 +12,12 @@ import json
 
 class Token(object):
     
-    def __init__(self, user_id, prefix, length, lifetime):
-        self.user_id = user_id
+    def __init__(self, user, prefix, length, lifetime, groups=[]):
+        self.user = user
         delta = timedelta(hours=lifetime)
         self.expire_on = datetime.now() + delta
         self.token = self.generate_token(prefix, length)
+        self.groups = groups
     
     def generate_token(self, prefix, length):
         charset = '0123456789abcdef'
@@ -27,6 +28,14 @@ class Token(object):
     def valid(self):
         return datetime.now() <= self.expire_on
 
+    def get_userid(self):
+        user_id = self.user.split('@')[0]
+        return user_id
+    
+    def get_user_realm(self):
+        realm = self.user.split('@')[1]
+        return realm
+   
     def __str__(self):
         return self.token
 
@@ -40,31 +49,42 @@ class Tokenizer(object):
         else:
             self.log = logging.getLogger(__name__)
 
-    def _new(self, user_id):
-        self.tokens[user_id] = Token(
-            user_id,
+    def _new(self, remote_user):
+        new_token = Token(
+            remote_user,
             self.config['prefix'],
             self.config.getint('length'),
             self.config.getint('lifetime'))
-        return self.tokens[user_id]
+        user_id = new_token.get_userid()
+        user_realm = new_token.get_user_realm()
+        new_token.groups = self._get_auth_groups(user_id, user_id, user_realm)
+        self.tokens[remote_user] = new_token
+        self.log.debug('New token %s for %s' % (new_token, remote_user))
+        return new_token
+    
+    def _get_auth_groups(self, tenant, user_id, realm):
+        groups = []
+        for grp in [ "{0}${1}:swift", "{0}${1}" ]:
+            groups.append(grp.format(tenant, user_id))
+        if realm in self.config['admin_realms'].split(','):
+            groups.append('.reseller_admin')
+        self.log.debug('Auth groups %s' % groups)
+        return groups
 
-    def get_or_create(self, user_id):
-        token = self.tokens.get(user_id)
-        if token:
-            if not token.valid():
-                self.log.debug('Token for %s invalid, renewing' % user_id)
-                token = self._new(user_id)
-        else:
-            token = self._new(user_id)
-        self.log.debug('Tokenizer get token for %s' % user_id)
+    def get_or_create(self, remote_user):
+        token = self.tokens.get(remote_user)
+        if not (token and token.valid()):
+            token = self._new(remote_user)
+        self.log.debug('Tokenizer get token for %s' % remote_user)
         return token
 
-    def exists(self, token):
-        for user_id in self.tokens:
-            if str(self.tokens[user_id]) == token:
-                self.log.debug('Token exists %s' % token)
-                return self.tokens[user_id]
-        self.log.debug('Token does not exist %s' % token)
+    def find(self, token_str):
+        for remote_user in self.tokens:
+            if str(self.tokens[remote_user]) == token_str:
+                token = self.tokens[remote_user]
+                self.log.debug('Token %s found, issued for %s with groups: %s' % (token, remote_user, token.groups))
+                return token
+        self.log.debug('Token does not exist %s' % token_str)
         return False
 
 class RadosGWAdminLocal:
@@ -115,6 +135,18 @@ class RadosGWAdminLocal:
         if key_type:
             params['key-type'] = key_type
         return self._call('subuser', 'create', params)
+    
+    def touch_user(self, user_id):
+        try:
+            self.get_user_info(user_id, user_id)
+            log.debug('User %s found' % user_id)
+        except Exception:
+            log.debug('Creating user %s' % user_id)
+            self.create_user(uid=user_id, display_name=user_id, tenant=user_id)
+            subuser = "%s:swift" % user_id
+            self.create_subuser(uid=user_id,
+                               subuser=subuser,
+                               tenant=user_id)
 
 class AuthHandler(http.server.BaseHTTPRequestHandler):
     
@@ -141,44 +173,25 @@ class AuthHandler(http.server.BaseHTTPRequestHandler):
                 storage_url += 'AUTH_%s' % user_id
             else:
                 storage_url += '/AUTH_%s' % user_id
+        log.debug('Storage url for %s is %s' % (user_id, storage_url))
         return storage_url
 
-    def get_user_groups(self, user_id, tenant):
-        groups ="{0}${1}:swift,{0}${1}".format(tenant, user_id)
-        log.debug('X-Auth-Groups %s' % groups)
-        return groups
-    
-    def touch_user(self, user_id):
-        try:
-            rgw.get_user_info(user_id, user_id)
-            log.debug('User %s found' % user_id)
-        except Exception:
-            log.debug('Creating user %s' % user_id)
-            rgw.create_user(uid=user_id, display_name=user_id, tenant=user_id)
-            subuser = "%s:swift" % user_id
-            rgw.create_subuser(uid=user_id,
-                               subuser=subuser,
-                               tenant=user_id)
-        
     def forbidden(self):
         self.send_response(403)
         self.end_headers()
     
-    def get_remote_user(self):
-        remote_user = self.headers.get('REMOTE_USER', '')
-        return remote_user.split('@')[0]
-
     def show_headers(self):
         for k in self.headers:
             log.debug('header %s: %s' % (k, self.headers.get(k)))
     
     def handle_auth(self, return_code=204):
-        user_id = self.get_remote_user()
-        if not user_id:
+        remote_user = self.headers.get('REMOTE_USER', '')
+        if not remote_user:
             return self.forbidden()
-        log.debug('Handle authenticated user %s' % user_id)
-        self.touch_user(user_id)
-        token = tokenizer.get_or_create(user_id)
+        log.debug('Handle authenticated user %s' % remote_user)
+        token = tokenizer.get_or_create(remote_user)
+        user_id = token.get_userid()
+        rgw.touch_user(user_id)
         self.send_response(return_code)
         self.send_header("X-Storage-Url", self.get_storage_url(user_id))
         self.send_header("X-Auth-Token", token)
@@ -192,14 +205,15 @@ class AuthHandler(http.server.BaseHTTPRequestHandler):
         return token
 
     def validate_token(self, return_code=200):
-        token = self.get_token_from_url()
-        t = tokenizer.exists(token)
-        if t:
+        token_req = self.get_token_from_url()
+        token = tokenizer.find(token_req)
+        if token:
             self.send_response(return_code)
-            self.send_header("X-Auth-Groups", self.get_user_groups(t.user_id, t.user_id))
+            groups = ','.join(token.groups)
+            self.send_header("X-Auth-Groups", groups)
             self.end_headers()
         else:
-            log.debug('Validate token not found %s' % token)
+            log.debug('Valid token not found %s' % token_req)
             self.forbidden()
 
     def do_GET(self):
@@ -219,7 +233,7 @@ if __name__ == '__main__':
     config.read('swift-http-auth.ini')
 
     rgw = RadosGWAdminLocal()
-    tokenizer = Tokenizer(config['token'], log)
+    tokenizer = Tokenizer(config['tokenizer'], log)
     httpd_config = (config['httpd']['host'], config.getint('httpd', 'port'))
 
     httpd = http.server.HTTPServer(httpd_config, AuthHandler)
